863751f4abefbe51cdbaa0f8191e797593ebfbc1
oops-accidental <smp5119@gmail.com>
2016-10-08 15:29:09 -0700

    Things
    That are bad

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controls : MonoBehaviour {
+
+    public enum controlEnum
+    {
+        aButton,
+        bButton,
+        yButton,
+        xButton,
+        dPadUp,
+        dPadDown,
+        dPadLeft,
+        dPadRight,
+        rightBumper,
+        leftBumper,
+        rightTrigger,
+        leftTrigger,
+        startButton,
+        backButton
+    };
+    public enum playerEnum
+    {
+        playerOne,
+        playerTwo,
+        playerThree,
+        playerFour,
+    };
+
+    Dictionary<controlEnum, string> controls = new Dictionary<controlEnum, string>()
+    {
+        { controlEnum.aButton, "aButton"},
+        { controlEnum.bButton, "bButton"},
+        { controlEnum.yButton, "yButton"},
+        { controlEnum.xButton, "xButton"},
+        { controlEnum.dPadUp, "dPadUp"},
+        { controlEnum.dPadDown, "dPadDown"},
+        { controlEnum.dPadLeft, "dPadLeft"},
+        { controlEnum.dPadRight, "dPadRight"},
+        { controlEnum.rightBumper, "rightBumper"},
+        { controlEnum.leftBumper, "leftBumper"},
+        { controlEnum.rightTrigger, "rightTrigger"},
+        { controlEnum.leftTrigger, "leftTrigger"},
+        { controlEnum.startButton, "startButton"},
+        { controlEnum.backButton, "backButton"},
+    };
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Controls.playerEnum player;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+	}
+
+    private void GetInput()
+    {
+
+    }
+}
